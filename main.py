02c5e1c82600787520a39b341158975bba28149c
cristian <gonzalez.resina@gmail.com>
2022-12-09 12:26:47 +0100

    correcciones

--- main.py
+++ main.py
@@ -2,27 +2,37 @@ import sqlite3
 conn = sqlite3.connect('colegio.db')
 cursor = conn.cursor()
 
-cursor.execute("CREATE TABLE alumnos(id INT AUTO_INCREMENT, nombre TEXT, apellidos TEXT)")
+cursor.execute("CREATE TABLE IF NOT EXISTS alumnos(nombre TEXT, apellidos TEXT)")
 
 def insertar_alumno(nombre, apellidos):
-    cursor.execute(f"INSERT INTO Alumnos(nombre, apellidos) VALUES('{nombre}', '{apellidos}')")
-    conn.commit()
+    
+        cursor.execute(f"INSERT INTO Alumnos(nombre, apellidos) VALUES('{nombre}', '{apellidos}')")
+        conn.commit()
+        
 
+def menu():
+    print('1-.VER ALUMNOS')
+    print('2-.INSERTAR ALUMNO')
+    opcion = int(input('Seleccione una opción: '))
+    match opcion:
+        case 1:
+            query = cursor.execute('SELECT rowid, nombre, apellidos FROM alumnos')
+            filas = cursor.fetchall()
+            print(filas)
+            menu()
+        case 2:
+            seguir= ''
+            while seguir != 'no':
+                nombre = input('Introduzca nombre: ')
+                apellido = input('introduzca apellido: ')
+                insertar_alumno(nombre, apellido)
+                seguir = input('¿Desea introducir otro (si/no)? ')
+            else:
+                menu()
+        case other:
+            print('Seleccione una opción correcta')
 
-print('1-.VER ALUMNOS')
-print('2-.INSERTAR ALUMNO')
-opcion = input('Seleccione una opción: ')
+menu()
 
-match opcion:
-    case 1:
-        query = cursor.execute('SELECT * FROM alumnos')
-        filas = cursor.fetchall(query)
-        print(filas)
-    case 2:
-        nombre = input('Introduzca nombre')
-        apellido = input('introduzca apellido')
-        insertar_alumno(nombre, apellido)
-    case other:
-        print('Seleccione una opción correcta')
 
 conn.close()
